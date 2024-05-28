f664ec73ee343e10999294d06c2a677e04882bc1
huy1412 <142949926+huy1412@users.noreply.github.com>
2024-05-28 12:07:20 +0700

    Role (#5)
    * done register
    
    * done login
    
    * done login and register 2
    
    * done Registe&&Login&&Changepassword

--- src/main/java/com/FPT/giahuy/Login/Register/controller/UserController.java
+++ src/main/java/com/FPT/giahuy/Login/Register/controller/UserController.java
@@ -4,9 +4,7 @@ import com.FPT.giahuy.Login.Register.dto.MemberDTO;
 import com.FPT.giahuy.Login.Register.dto.UserDTO;
 import com.FPT.giahuy.Login.Register.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
