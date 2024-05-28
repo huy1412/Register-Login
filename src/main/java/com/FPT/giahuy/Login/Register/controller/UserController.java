232adab42c4d21370e3d7afa95eb118deb52a509
unknown <huytgse170167@fpt.edu.vn>
2024-05-28 12:10:59 +0700

    Register&&Login&&Changepassword

--- src/main/java/com/FPT/giahuy/Login/Register/controller/UserController.java
+++ src/main/java/com/FPT/giahuy/Login/Register/controller/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 
     @PutMapping("/changepassword/{id}")
     public UserDTO updatePasswordUser(@PathVariable("id") int id, @RequestBody UserDTO userDTO) {
-        UserDTO user = userService.changePassword(id, userDTO);
-        return user;
+        UserDTO user1 = userService.changePassword(id, userDTO);
+        return user1;
     }
 }
